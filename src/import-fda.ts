0f2813bbed8ddc0d0ae44a1b059e052320f4f8f9
Brandon Egger <me@brandonegger.com>
2024-04-03 12:44:18 -0500

    add download dir path builder

--- src/import-fda.ts
+++ src/import-fda.ts
@@ -1,5 +1,9 @@
 import { ENV } from "./env";
+import path from "node:path";
 
-console.log(ENV.FOOD_DATA_DOWNLOAD);
+const downloadDir = path.join(ENV.TEMP_DATA_DIR, "food-downloads");
+console.log(downloadDir);
 
 // TODO: Download food, unzip, import to DB
+
+// DELETE FOOD DOWNLOAD DIR
