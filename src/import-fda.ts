bbb14a1a07ca0054e6c2ded6eb4d7dd979415122
Brandon Egger <me@brandonegger.com>
2024-04-03 13:23:13 -0500

    add download script

--- src/import-fda.ts
+++ src/import-fda.ts
@@ -1,12 +1,55 @@
 import { ENV } from "./env";
 import path from "node:path";
 import pino from "pino";
+import https from "https";
+import fs from "node:fs";
 
 const logger = pino();
 
 const downloadDir = path.join(ENV.TEMP_DATA_DIR, "food-downloads");
-logger.info({ downloadDir }, "Setting up temp directory for downloads.");
 
-// TODO: Download food, unzip, import to DB
+/**
+ *
+ * @returns Path to downloaded file.
+ */
+async function download(
+  url: string,
+  outDir: string = downloadDir,
+): Promise<string> {
+  const zipPath = path.join(downloadDir, "food-data.zip");
+  const dataZip = fs.createWriteStream(zipPath);
 
-// DELETE FOOD DOWNLOAD DIR
+  logger.info({ outDir }, "Setting up temp directory for downloads.");
+  if (!fs.existsSync(outDir)) {
+    logger.info("Output directory missing, creating missing directory.");
+    fs.mkdirSync(outDir, { recursive: true });
+  }
+
+  const request = new Promise((res, rej) => {
+    https.get(url, function (response) {
+      logger.info({ downloadUrl: url }, "Beginning download");
+      response.pipe(dataZip);
+
+      dataZip.on("finish", () => {
+        dataZip.close();
+        logger.info({ zipPath }, "Download complete.");
+        res(true);
+      });
+
+      dataZip.on("error", (error) => {
+        rej(error);
+      });
+    });
+  });
+
+  await request;
+
+  return zipPath;
+}
+
+async function main() {
+  const zipPath = await download(ENV.FOOD_DATA_DOWNLOAD);
+  logger.info(zipPath);
+}
+
+main().catch((err) => logger.error(err));
