f6c842e0e25358fc54191e864ce7abaa93209c1c
Brandon Egger <me@brandonegger.com>
2024-04-16 15:00:51 -0500

    remove unused env variables

--- src/env.ts
+++ src/env.ts
@@ -7,9 +7,6 @@ const EnvSchema = z.object({
   PORT: z.coerce.number(),
   OPENAI_API_KEY: z.string(),
   TEMP_DATA_DIR: z.string(),
-  MONGO_USER: z.string(),
-  MONGO_PASSWORD: z.string(),
-  MONGO_PORT: z.coerce.number(),
   MONGO_URL: z.string(),
   FOOD_DATA_API_KEY: z.string(),
 });
@@ -19,9 +16,6 @@ function readEnv() {
     PORT: process.env.PORT,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     TEMP_DATA_DIR: process.env.TEMP_DATA_DIR,
-    MONGO_USER: process.env.MONGO_USER,
-    MONGO_PORT: process.env.MONGO_PORT,
-    MONGO_PASSWORD: process.env.MONGO_PASSWORD,
     MONGO_URL: process.env.MONGO_URL,
     FOOD_DATA_API_KEY: process.env.FOOD_DATA_API_KEY,
   });
