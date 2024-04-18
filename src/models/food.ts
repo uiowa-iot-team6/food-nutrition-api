d81bdd18a94cbb5437d8e5e75302d22533beed2e
Joslin Some <joslin-some@uiowa.edu>
2024-04-17 20:05:44 -0500

    importing mongoose

--- src/models/food.ts
+++ src/models/food.ts
@@ -1,4 +1,4 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
 import { IUSDAFood } from "../utils/usda-api";
 
 export const FoodNutrientSchema = new Schema({
